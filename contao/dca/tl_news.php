fa2b972ecc6e6d57a5b49cadf90c74bf71c0a68a
cboelter <christopher@boelter.eu>
2014-11-27 10:12:05 +0100

    added hook class

--- contao/dca/tl_news.php
+++ contao/dca/tl_news.php
@@ -8,5 +8,6 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['hidedate'] = array
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['hidedate'],
     'exclude'   => true,
     'inputType' => 'checkbox',
+    'eval'      => array('tl_class' => 'w50 m12'),
     'sql'       => "char(1) NOT NULL default ''",
 );
